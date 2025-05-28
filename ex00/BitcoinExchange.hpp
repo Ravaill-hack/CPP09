86161e7e3eaf04d5877fbc42660987d064a5788a
lmatkows <lmatkows@student.42perpignan.fr>
2025-05-28 13:55:01 +0200

    en cours de commencement

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+class BitcoinExchange
+{
+	public:
+
+	private:
+};
+
+#endif
